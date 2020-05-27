603b27644d346522904ec08a2df8b9d439c97c3d
Yuri Edward <nicolas1.fraysse@epitech.eu>
2020-05-27 15:28:48 +0200

    Atempt 3 at getting a path

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+//const fs = require('fs');
+
+//const homedir = require('os').homedir();
+const scriptDir = __dirname + "/script/";
 
 core.exportVariable("TERM", "xterm-256color");
 
 if (process.platform === "win32") {
     //Setup windows
-    core.addPath("./script/win32/");
+    core.addPath(scriptDir + "win32");
 } else if (process.platform === "darwin") {
     //Setup macosx
     exec.exec("brew install expect");
-    core.addPath("./script/darwin/");
+    core.addPath(scriptDir + "darwin");
 } else {
     //Setup linux
-    core.addPath("./script/linux/");
+    core.addPath(scriptDir + "linux");
 }
