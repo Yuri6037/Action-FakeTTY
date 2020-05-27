68427977c68255d02770402f838a369f0af9c935
Yuri Edward <nicolas1.fraysse@epitech.eu>
2020-05-27 12:44:15 +0200

    Attempt at fixing alias not appearing

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const core = require('@actions/core');
-const { exec } = require('child_process');
+const exec = require('@actions/exec');
 
 core.exportVariable("TERM", "xterm-256color");
 
 if (process.platform === "win32") {
     //Setup windows
-    exec("Set-Alias faketty Invoke-Expression \"$args\"");
+    exec.exec("Set-Alias faketty Invoke-Expression \"$args\"");
 } else if (process.platform === "darwin") {
     //Setup macosx
-    exec("brew install expect");
-    exec("alias faketty=\"unbuffer $*\"");
+    exec.exec("brew install expect");
+    exec.exec("alias faketty=\"unbuffer $*\"");
 } else {
     //Setup linux
-    exec("alias faketty=\"script -c -e \\\"$*\\\"\"");
+    exec.exec("alias faketty=\"script -c -e \\\"$*\\\"\"");
 }
